d2a8d2c72e4c3894eab8fef9d017a6794a38d0e7
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-05-11 22:38:22 +0900

    inputで取得

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し初期化する
+  const inputText = document.getElementById("js-add-txt").value;
+  document.getElementById("js-add-txt").value = "";
+
+  //li生成
+  const li = document.createElement("li");
+  li.className = "flex-box";
+  console.log(li);
+
+  //div生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+  console.log(p);
+};
+
+document
+  .getElementById("js-add-btn")
+  .addEventListener("click", () => onClickAdd());
